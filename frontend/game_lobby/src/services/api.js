d14be2ea04ee1a39594fdaba16be16b909bf9554
Oluwatunmise Alimi <oluwatunmiselaimi67@gmail.com>
2025-08-22 18:48:57 +0100

    deployment

--- frontend/game_lobby/src/services/api.js
+++ frontend/game_lobby/src/services/api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Attach JWT token to all requests
+
+// Automatically attach JWT token to all requests
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
-// Auth APIs
+// -------- Auth APIs --------
 export const registerUser = async (username, password) => {
   const res = await api.post("/auth/register", { username, password });
   return res.data;
@@ -22,35 +23,32 @@ export const loginUser = async (username, password) => {
   return res.data;
 };
 
-// Game APIs
-//console.log("Token in localStorage:", localStorage.getItem("token"));
+// -------- Game APIs --------
 export const joinGame = async (chosenNumber) => {
   const token = localStorage.getItem("token");
   if (!token) throw new Error("User not logged in");
 
-  const res = await axios.post(
-    "http://localhost:5000/api/session/join",
+  const res = await api.post(
+    "/session/join",
     { chosenNumber },
     { headers: { Authorization: `Bearer ${token}` } }
   );
-
   console.log("Join response:", res.data);
   return res.data;
 };
-
 
 export const getGameResult = async (sessionId) => {
   const res = await api.get(`/session/result?sessionId=${sessionId}`);
   return res.data;
 };
 
-// Leaderboard
+// -------- Leaderboard --------
 export const fetchTopPlayers = async () => {
   const res = await api.get("/leaderboard/");
   return res.data;
 };
 
-// Leave session
+// -------- Leave session --------
 export const leaveSession = async (sessionId) => {
   const res = await api.post("/session/leave", { sessionId });
   return res.data;
